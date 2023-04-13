c6b893778f0c4b4e6b940a335c227d792153e5d9
박민성 <altp12@miris.co.kr>
2023-04-13 13:15:45 +0900

    Initial commit

---
+++ src/test/java/com/miris/web/WebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miris.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
